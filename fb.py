d6ddf15e219fd78f70fdcfcff5e6b53e2415e180
Ajith Anand <ajithanand303@gmail.com>
2023-01-04 02:16:36 +0530

    updated

--- fb.py
+++ fb.py
@@ -1,18 +1,18 @@
 import os
-import facebook,json,requests
-import  pandas as pd
+import requests
+import pandas as pd
+
 
 class Facebook:
     def __init__(self):
         self.__access_token = os.getenv('facebook_token')
-        self.graph = facebook.GraphAPI(self.__access_token)
         self.url = 'http://graph.facebook.com/v15.0/me/posts?fields=reactions.summary(total_count).type(LIKE),comments.summary(total_count)&access_token=%s' % self.__access_token
+
     def get_posts_data(self):
         res = requests.get(self.url)
-        # print(res.status_code)
-        # return None
-        return res.json() if res.status_code==200 else None
-    def cleanup(self,data):
+        return res.json() if res.status_code == 200 else None
+
+    def cleanup(self, data):
         new_dict = {}
         count = 0
         for d in data['data']:
@@ -20,12 +20,15 @@ class Facebook:
             new_dict[count]['id'] = d['id']
             new_dict[count]['likes'] = d['reactions']['summary']['total_count']
             new_dict[count]['comments'] = d['comments']['summary']['total_count']
-            count+=1
+            count += 1
         return new_dict
-    def gen_DataFrame(self,data):
-        df = pd.DataFrame.from_dict(data,orient='index')
-        df.columns = ['id','likes','comments']
+
+    def gen_DataFrame(self, data):
+        df = pd.DataFrame.from_dict(data, orient='index')
+        df.columns = ['id', 'likes', 'comments']
         return df
+
+
 def main():
     fb = Facebook()
     data = fb.get_posts_data()
@@ -35,7 +38,7 @@ def main():
     data = fb.cleanup(data)
     df = fb.gen_DataFrame(data)
     print(df)
-    
+
 
 if __name__ == '__main__':
     main()
